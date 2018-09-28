fb936a017d825d719552391af0c7a54bdc880ebc
Jennifer Balangue <jfbalangue@yahoo.com>
2018-09-27 23:57:09 -0400

    add navbar component

--- clickygameapp/src/components/Jumbotron/Jumbotron.js
+++ clickygameapp/src/components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import "./Jumbotron.css";
 
 const Jumbotron = () => (
 	<header className = "header">
-		<h1>Dragonball Clicky Game!</h1>
+		<h1>Dragon Ball Clicky Game!</h1>
 		<h5>Click on any image ONCE to earn a point but TWICE you lose all your points. Click all 12 and you WIN.</h5>
 	</header>
 );
